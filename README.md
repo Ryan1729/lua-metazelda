2cf459f58b00919496c197b4cad7b7943c3580df
Ryan1729 <Ryan1729@gmail.com>
2015-08-13 18:56:53 -0600

    still WIP
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lua-metazelda
 puzzle genetration in lua based on https://github.com/tcoxon/metazelda
+
+Still WIP
---
+++ generateDungeon.lua
@@ -0,0 +1,13 @@
+local function generateDungeon (constraints, seed)
+  seed = seed or os.time()
+  
+  math.randomseed(seed)
+  
+  local result = {}
+  
+  
+  
+  return result
+end
+
+return generateDungeon
